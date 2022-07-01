b21346e84fcb16431d0cccb542624051da273eca
LegendeJJ <dalienjj@gmail.com>
2022-07-01 19:29:06 +0200

    Grid is now dynamic.
    Gridelements are created by selecting the rangeslider.

--- script.js
+++ script.js
@@ -1,9 +1,67 @@
-const grid = document.querySelectorAll(".griditem")
+const sizevalue = document.getElementById("sizevalue");
+const sizeslider = document.getElementById("sizeslider");
+const grid = document.getElementById("grid")
 
-grid.forEach(element => {
-    element.addEventListener("mousedown", (el) => {
-        console.log(el.target)
-        el.target.style.backgroundColor = "black"
-    })
-});
+let currentColor = "#333333"
+let currentSize = 16;
+
+
+function setCurrentSize(newSize) {
+    currentSize = newSize;
+}
+
+
+
+sizeslider.onchange = (e) => changeSize(e.target.value)
+sizeslider.onmousemove = (e) => updateSizeValue(e.target.value)
+
+function updateSizeValue(value) {
+    sizevalue.innerHTML = `${value} x ${value}`
+
+}
+
+function clearGrid() {
+    grid.innerHTML = "";
+}
+
+function loadGrid() {
+    clearGrid();
+    setupgrid(currentSize)
+}
+
+function changeSize(value) {
+    setCurrentSize(value);
+    updateSizeValue(value)
+    loadGrid(value)
+}
+
+
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
+
+function changeColor(e) {
+    if (e.type === 'mouseover' && !mouseDown) return
+    e.target.style.backgroundColor = currentColor
+}
+
+
+function setupgrid(size) {
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
+
+    for (let i = 0; i < size * size; i++) {
+        const gridelement = document.createElement("div");
+        gridelement.classList.add("griditem")
+        gridelement.addEventListener("mouseover", changeColor)
+        gridelement.addEventListener("mousedown", changeColor)
+        grid.appendChild(gridelement)
+    }
+}
+
+
+window.onload = () => {
+    loadGrid();
+}
+
 
